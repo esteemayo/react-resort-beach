d73bba5cc996211d1e9f32799526098f543351fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-14 02:52:20 +0100

    fix: change file directory

--- src/components/RoomFilter.jsx
+++ src/components/RoomFilter.jsx
@@ -1,5 +1,5 @@
 import Title from './Title';
-import { useGlobalContext } from 'context/RoomContext';
+import { useGlobalContext } from 'context/room/RoomContext';
 
 const getUnique = (items, value) => {
   return [...new Set(items.map((item) => item[value]))];
