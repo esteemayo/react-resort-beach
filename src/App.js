e12191fbac79c9c5bd705d2e17173166fa4ca942
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-14 02:48:55 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home, Rooms, Error, SingleRoom } from 'pages';
-import NavBar from 'components/NavBar';
 
 import './App.css';
 
